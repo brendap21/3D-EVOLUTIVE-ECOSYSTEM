61e4fb458232e729a6ae0bcd4b6c16d61a5eeba7
brendap21 <bpna16100224@gmail.com>
2025-12-11 15:59:19 -0600

    Stop tracking build.bat and ANIMATION_IMPROVEMENTS.md; add ignores

--- build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Build script for 3D EVOLUTIVE ECOSYSTEM (Windows cmd.exe)
-REM Creates bin directory and compiles all .java from src into bin.
-if not exist bin mkdir bin
-dir /s /b src\*.java > sources.txt
-javac -d bin @sources.txt
-if exist sources.txt del sources.txt
-echo Build finished.
-pause
